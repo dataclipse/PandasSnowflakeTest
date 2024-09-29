eaade24da30865081a4f3044419b118135dc2d78
rhiltner <ryan.hiltner@gmail.com>
2024-09-29 13:50:45 -0500

    Created Tables and Pushed Altered Data to Snowflake DB

--- SnowflakeETLSample.py
+++ SnowflakeETLSample.py
@@ -1,5 +1,10 @@
-import pandas as panda_data
+import pandas as pandas_data
 import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
+import warnings
+
+#Ignore UserWarnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 # Snowflake connection parameters
 SNOWFLAKE_ACCOUNT = 'QPOWBAL-VL87933'
@@ -8,7 +13,7 @@ SNOWFLAKE_PASSWORD = '*********' #removed before pushed to github
 SNOWFLAKE_WAREHOUSE = 'COMPUTE_WH'
 SNOWFLAKE_DATABASE = 'COVID_DATA'
 SNOWFLAKE_SCHEMA = 'COVID_DATA_SCHEMA'
-SNOWFLAKE_ROLE = 'COVIDDATA_SELECTOR'
+SNOWFLAKE_ROLE = 'COVID_DATA_RW'
 
 print("Connecting...\n")
 # Establish a Snowflake connection
@@ -23,11 +28,14 @@ conn = snowflake.connector.connect(
     IgnoreCase=True
 )
 
+#Create Cursor for the Connection
+cursor = conn.cursor()
+
 # Query to select all data from a specific table
 query = "SELECT COUNTRY, CASES_NEW, DEATHS_NEW, DATE FROM COVID19_WHO_SITUATION_REPORTS;" 
 
 # Fetch data from Snowflake into a Pandas DataFrame
-df = panda_data.read_sql(query, conn)
+df = pandas_data.read_sql(query, conn)
 
 # Check for missing data
 print("Number of null values in dataset: \n") 
@@ -45,17 +53,58 @@ df = df[df['DEATHS_NEW'] >= 0]
 
 # Convert the Date Column to the proper Date format
 print("Convert Date Column to Date format\n")
-df['DATE'] = panda_data.to_datetime(df['DATE'])
+df['DATE'] = pandas_data.to_datetime(df['DATE'])
 
 #Display the top 10 countries sorted by new case count for the date of 2020-08-09
 print("Top 10 Countries sorted by case count for the date of 2020-08-09\n")
 top_countries = df[df['DATE'] == '2020-08-09'].sort_values(by='CASES_NEW', ascending=False).head(10)
-print(top_countries, "\n")
+
+#Convert Date field back to Snowflake compatible Date
+top_countries['DATE'] = top_countries['DATE'].dt.date
+#print(top_countries, "\n")
 
 #Sum all the new cases and new deaths grouped by country
 print("Sum of all New Cases and New Deaths Grouped by Country...\n")
 total_by_country = df.groupby('COUNTRY')[['CASES_NEW', 'DEATHS_NEW']].sum().reset_index()
-print(total_by_country, "\n")
+#print(total_by_country, "\n")
+
+#Create New Tables
+print("Creating new tables...\n")
+create_table_query0 = """
+CREATE OR REPLACE TABLE TOP_10_COUNTRIES_20200809 (
+    COUNTRY VARCHAR(16777216),
+    CASES_NEW NUMBER(38,0),
+    DEATHS_NEW NUMBER(38,0),
+    DATE DATE
+);
+"""
+create_table_query1 = """
+CREATE OR REPLACE TABLE COUNTRY_CASE_DEATH_COUNT (
+    COUNTRY VARCHAR(16777216),
+    CASES_NEW NUMBER(38,0),
+    DEATHS_NEW NUMBER(38,0)
+);
+"""
+cursor.execute(create_table_query0)
+cursor.execute(create_table_query1)
+
+#Write Data to Snowflake DB
+success, nchunks, nrows, _ = write_pandas(conn, top_countries, 'TOP_10_COUNTRIES_20200809')
+
+#Verify the result
+if success:
+    print("Successfully inserted ", nrows, " rows into 'TOP_10_COUNTRIES_20200809'.")
+else:
+    print("Data insertion failed.")
+    
+#Write Data to Snowflake DB    
+success, nchunks, nrows, _ = write_pandas(conn, total_by_country, 'COUNTRY_CASE_DEATH_COUNT')
+
+#Verify the result
+if success:
+    print("Successfully inserted ", nrows, " rows into 'COUNTRY_CASE_DEATH_COUNT'.")
+else:
+    print("Data insertion failed.")
 
 # Close the Snowflake connection
 conn.close()
